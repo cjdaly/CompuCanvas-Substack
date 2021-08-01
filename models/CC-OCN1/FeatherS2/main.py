dd8e7b2de39d2258fb32be93e6f1a7e66ef55501
Chris <cjdaly@locosoft.net>
2021-07-31 18:37:42 -0700

    green and submarine

--- models/CC-OCN1/FeatherS2/main.py
+++ models/CC-OCN1/FeatherS2/main.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import board, busio, time
+import board, busio, digitalio, time, neopixel
 import adafruit_dotstar, adafruit_mcp4728
 
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -30,17 +30,34 @@ mcp4728.channel_a.value = 50000
 dot = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
 dot[0]=(0,33,0)
 
-i = 0
+px = neopixel.NeoPixel(board.IO17, 2)
+px.fill((0,0,33))
+
+d0 = digitalio.DigitalInOut(board.IO18)
+d0.direction=digitalio.Direction.INPUT
+d0.pull=digitalio.Pull.UP
+
+d1 = digitalio.DigitalInOut(board.IO12)
+d1.direction=digitalio.Direction.INPUT
+d1.pull=digitalio.Pull.UP
 
 while True:
-    mcp4728.channel_a.value = 48000 + (i*2000)
-    dot[0]=(0,33,0)
-    time.sleep(1)
-    dot[0]=(0,0,33)
-    time.sleep(1)
-    i += 1
-    if i >=4:
-        i = 0
-
-
-
+    v_base=0 ; v0=0 ; v1=0
+    if d0.value:
+        px[0] = (0,0,33)
+        dot[0]=(0,0,33)
+    else:
+        px[0] = (0,33,0)
+        dot[0]=(0,33,0)
+        v_base = 50000 ; v0 = 1000
+    #
+    if d1.value:
+        px[1] = (0,0,33)
+        dot[0]=(0,0,33)
+    else:
+        px[1] = (0,33,0)
+        dot[0]=(10,0,20)
+        v_base = 50000 ; v1 = 2000
+    #
+    mcp4728.channel_a.value = v_base + v0 + v1
+    time.sleep(0.05)
